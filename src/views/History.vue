168aba550efc7a696013c53bdb33b7db431fc21c
Vadim I. <vadign00@gmail.com>
2025-04-28 12:16:37 +0200

    Add history fallback

--- src/views/History.vue
+++ src/views/History.vue
@@ -7,11 +7,18 @@ const history = (await useFetch('receipts/history')).data.data;
 </script>
 
 <template>
-  <div>
+  <div v-if="history.length">
     <div class="h2">История посещений</div>
     <Icon name="separator" class="separator" />
     <div class="history-items scrollable">
       <HistoryItem v-for="item of history" v-bind="item" />
+    </div>
+  </div>
+  <div v-else>
+    <div class="h2">История посещений</div>
+    <div class="history__empty">
+      <Icon name="alien" />
+      <span>Вы еще не отсканировали ни одного чека. Пора скорее это исправить!</span>
     </div>
   </div>
 </template>
@@ -31,5 +38,18 @@ const history = (await useFetch('receipts/history')).data.data;
   flex-flow: column;
   gap: 16px;
   padding: 20px var(--side-padding);
+}
+
+.history {
+  &__empty {
+    display: grid;
+    gap: 16px;
+    justify-items: center;
+    color: var(--theme-60);
+    font-size: 16px;
+    font-weight: 700;
+    margin: auto;
+    text-align: center;
+  }
 }
 </style>
